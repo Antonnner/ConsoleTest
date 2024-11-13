fc480012cb345691f6a1951ccfb678649aa74d52
Kirill <Kirill@DESKTOP-DJNCTT5>
2024-11-13 18:28:38 +0300

    Добавьте файлы проекта.

---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.IO;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = "MLstartConfig.json";
+            var content = @"{ ""Test"": ""123""}";
+            File.WriteAllText(fileName, content);
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile(fileName).Build();
+
+            string testConf = configuration["Test"];
+            Console.WriteLine(testConf);
+        }
+    }
+}
